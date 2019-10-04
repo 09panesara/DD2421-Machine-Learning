bc8f1f2ab1dfd4c6c10ab8db2f77bf198169e75e
Markus Zvonek <e1525726@student.tuwien.ac.at>
2019-10-04 12:05:52 +0200

    continued with implementation, started to fix bugs

--- lab2_svm/svm.py
+++ lab2_svm/svm.py
@@ -9,6 +9,10 @@ K = None # precomputed Kernel function
 P = None # precomputed ti tj K[i,j] matx
 non_zero_as, non_zero_xs, non_zero_ts = None, None, None # support vectors
 b = None
+classA, classB = None, None
+
+#TODO find bug for wrong decision boundary
+# with simple data points and control calculations made by hand compute_objective_matx(), objective() and kernel() seem to be ok
 
 def compute_objective_matx(t, x):
     ''' Precomputes P
@@ -19,42 +23,59 @@ def compute_objective_matx(t, x):
     K = np.asarray([[kernel(x[i], x[j]) for j in range(n)] for i in range(n)])
     P = np.asarray([[t[i] * t[j] * K[i, j] for j in range(n)] for i in range(n)])
 
-def generate_data(plot=False):
-    global xs, ts
-    np.random.seed(100) # generate same data each time
-    classA = np.concatenate((np.random.randn(10, 2) * 0.2 +[1.5, 0.5], np.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
-    classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
-    if plot:
-        plot_data(classA, classB)
+def generate_data():
+    global xs, ts, classA, classB, N
+    # TODO remove comments after debugging is finished
+    #np.random.seed(100) # generate same data each time
+    #classA = np.concatenate((np.random.randn(10, 2) * 0.2 +[1.5, 0.5], np.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
+    #classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+    # BEGIN REMOVE
+    classA = np.array([(-4, 0)])
+    classB = np.array([(4, 0)])
+    # END REMOVE
     inputs = np.concatenate((classA, classB))
     targets = np.concatenate((np.ones(classA.shape[0]), -np.ones(classB.shape[0])))
     N = inputs.shape[0]  # Number of rows (samples)
     # randomly reorder samples
-    permute = list(range(N))
-    random.shuffle(permute)
-    inputs = inputs[permute, :]
-    targets = targets[permute]
+    #TODO remove comments after debugging is finished
+    #permute = list(range(N))
+    #random.shuffle(permute)
+    #inputs = inputs[permute, :]
+    #targets = targets[permute]
     xs = inputs
     N = len(xs)
     ts = targets
+    compute_objective_matx(ts, xs)
 
 def plot_data(classA, classB):
     plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
     plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
     plt.axis('equal')  # Force same scale on both axes plt.savefig(’svmplot.pdf’) # Save a copy in a file plt .show() # Show the plot on the screen
-    plt.savefig('plots/svmplot.pdf')
+
+    # plot decision boundary
+    xgrid = np.linspace(-5, 5)
+    ygrid = np.linspace(-4, 4)
+
+    grid = np.array([[indicator_function(np.array(x, y)) for x in xgrid] for y in ygrid])
+    plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0), colors=('red', 'black', 'blue'), linewidths=(1, 3, 1))
+
+    plt.savefig('plots/svmplot-test.pdf')
     plt.show()
 
 def _euclidean_squared(x, y):
-    return np.sum([(x[i]-y[i])**2 for i in range(len(x))])
+    x = np.array(x)
+    y = np.array(y)
+    return np.sum((x-y)**2)
 
 def kernel(x, y, kernel_type='linear', p=2, sigma=0.01):
+    #print(x, y)
     if kernel_type == 'linear':
+        #print(np.dot(x, y))
         return np.dot(x, y)
     elif kernel_type == 'polynomial':
         return (np.dot(x, y) + 1)**p
     elif kernel_type == 'rbf': # radial basis function
-        return math.e**(-_euclidean_squared(x,y)/(2*sigma**2))
+        return math.e**(-_euclidean_squared(x, y)/(2*sigma**2))
     else:
         raise Exception(f'Cannot recognise kernel type {kernel_type}')
 
@@ -62,19 +83,27 @@ def objective(alpha):
     ''' Computes objective function using kernel trick (see eqn 4 in lab notes)
     '''
     n = len(alpha)
-    alpha_mat = np.array([a_i*alpha for a_i in alpha])
+    #alpha_mat = np.array([a_i*alpha for a_i in alpha])
     # TODO remove for testing purposes only
-    correct_sum = np.sum([[alpha[i] * alpha[j] * P[i,j] for j in range(n)] for i in range(n)])
-    assert correct_sum == np.sum(np.dot(P, alpha_mat)) - np.sum(alpha)
-    # END OF REMOVE
-    minimize_fn = 1 / 2 * np.sum(np.dot(P, alpha_mat)) - np.sum(alpha)
+    #print(n, alpha, P)
+    part_1 = np.sum([[alpha[i] * alpha[j] * P[i,j] for j in range(n)] for i in range(n)])
+    part_2 = np.sum(alpha)
+    correct_sum = part_1 - part_2
+
+    # target_sum = np.sum(np.dot(P, alpha_mat)) - np.sum(alpha)
+    # if correct_sum != target_sum and abs(correct_sum) > 10**-6 and abs(target_sum) > 10**-6:
+    #     print('correct sum', correct_sum)
+    #     print('target sum', target_sum)
+    #     assert correct_sum == np.sum(np.dot(P, alpha_mat)) - np.sum(alpha)
+    # # END OF REMOVE
+    # minimize_fn = 1 / 2 * np.sum(np.dot(P, alpha_mat)) - np.sum(alpha)
+    minimize_fn = 0.5 * correct_sum
     return minimize_fn
 
 def zerofun(alpha):
-    # TODO does this receive alpha or t vector?
     return np.sum(np.dot(alpha, ts))
 
-def compute_b(s, target_s, alpha):
+def compute_b(alpha, t, x):
     ''' computes threshold value b
         s = support vector
         x =
@@ -82,10 +111,11 @@ def compute_b(s, target_s, alpha):
         alpha =
         t = target classes of x?
     '''
-    global ts
+    s = x[0]
+    target_s = ts[0]
     alpha = np.array(alpha)
-    alpha_ts = alpha * ts # compute a_i * t_i vector [a_1 * t_1, a_2 * t_2, ...]
-    return np.sum(np.dot(alpha_ts, kernel(s, xs).T)) - target_s
+    alpha_ts = alpha * t # compute a_i * t_i vector [a_1 * t_1, a_2 * t_2, ...]
+    return np.sum([np.dot(alpha_ts, kernel(s, xi)) for xi in x]) - target_s
 
 def extract_non_zero_alphas(alpha, x):
     ''' extract alpha values which are non-zero and returns the corresponding alphas, data points and target values '''
@@ -95,22 +125,31 @@ def extract_non_zero_alphas(alpha, x):
     t_s = ts[non_zero_indices]
     return a_s, x_s, t_s
 
-def indicator_function(s, target_s, alpha):
+def indicator_function(s):
     ''' computes sum over non-zero values
         s = new datapoint to be classified
     '''
     alpha_ts = non_zero_as * non_zero_as
-    return np.sum(np.dot(alpha_ts, kernel(s, non_zero_xs).T)) - b
+    return np.sum(np.dot(alpha_ts, kernel(s, non_zero_xs))) - b
 
-def svm_classifier():
+def svm_classifier(plot=False):
     global non_zero_as, non_zero_xs, non_zero_ts, b
     # TODO
-    # b = compute_b(s, target_s, alpha)
-    # non_zero_as, non_zero_xs, non_zero_ts = extract_non_zero_alphas(alpha, xs)
     generate_data()
     C = 0.1 # TODO change value. Lower = rely more on slack = better for noisier datasets
-    ret = minimize(objective,  np.zeros(N), bounds=[(0, C) for x in xs], constraints=zerofun)
-    alpha = ret['x']
+    constraint = {'type': 'eq', 'fun': zerofun}
+    ret = minimize(objective,  np.zeros(N), bounds=[(0, None) for x in xs], constraints=constraint)
+    if ret['success']:
+        alpha = ret['x']
+        non_zero_as, non_zero_xs, non_zero_ts = extract_non_zero_alphas(alpha, xs)
+        b = compute_b(non_zero_as, non_zero_ts, non_zero_xs)
+        print('b', b)
+        print('found alpha', alpha)
+
+        if plot:
+            plot_data(classA, classB)
+    else:
+        print('Unable to separate data')
 
 if __name__ == '__main__':
-    generate_data()
+    svm_classifier(True)
